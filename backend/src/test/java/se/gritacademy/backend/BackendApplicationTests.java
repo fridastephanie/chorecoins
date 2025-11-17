b04d7062b56cbdb714e17004e4d1a941ca490169
Frida Hassel <frida.hassel@live.se>
2025-11-17 21:13:29 +0100

    Set up Spring Boot backend project with external database environment variables

---
+++ backend/src/test/java/se/gritacademy/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.gritacademy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
